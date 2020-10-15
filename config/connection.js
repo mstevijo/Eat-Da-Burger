06725840bc9654f6231d99d0e8615aa444fff323
Stevi Jo Malinowski <mstevijo@yahoo.com>
2020-10-14 21:33:40 -0700

    Updated connections.js file

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+var mysql = require("mysql");
+connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Nylea$0$0",
+    database: "burger_db"
+});
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+module.exports = connection;
